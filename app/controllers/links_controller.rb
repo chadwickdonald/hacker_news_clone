f915a96cc707c4390a8bc3ea80ec7480aba1c2f0
Apprentice <apprentice@devbootcamp.com>
2012-07-23 12:31:07 -0700

    added a bunch of stuff related to Links

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,23 @@
+class LinksController < ApplicationController
+  def index
+    @links = Link.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
