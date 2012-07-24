3c8e59f58ce0f5514177e24d0240b7f05f54c9a0
Chadwick Bidwell chadwickbidwell@gmail.com + Oleg Trofimov otrofimo@gmail.com <apprentice+cb+ot@devbootcamp.com>
2012-07-24 16:26:57 -0700

    user can sign in and out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  helper_method :current_user, :signed_in?
+
   def current_user
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # def current_user=(user)
+  #   @current_user = user
+  # end
+
+  def signed_in?
+    !current_user.nil?
   end
 end
